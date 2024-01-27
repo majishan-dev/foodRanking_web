deea805e0e242df3f8086853e8d9430958b63c03
hiropau <hiropay28@gmail.com>
2024-01-27 11:52:52 +0900

    model追加

--- api_code/instagram_get/instagram_api_get_data.py
+++ api_code/instagram_get/instagram_api_get_data.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 
+
 def load_settings(file_name):
     """設定ファイルを読み込んで辞書として返す。"""
     base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,45 +10,49 @@ def load_settings(file_name):
     with open(file_path, "r") as file:
         return json.load(file)
 
+
 def get_ig_hash_id(business_id, hash_tag_name, access_token):
     """InstagramのハッシュタグIDを取得する。"""
-    url = f'https://graph.facebook.com/v17.0/ig_hashtag_search?user_id={business_id}&q={hash_tag_name}&access_token={access_token}'
+    url = f"https://graph.facebook.com/v17.0/ig_hashtag_search?user_id={business_id}&q={hash_tag_name}&access_token={access_token}"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        return data['data'][0]['id']
+        return data["data"][0]["id"]
     except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
+        print(f"HTTP error occurred: {http_err}")
     except Exception as error:
-        print('Instagram APIのリクエスト中にエラーが発生しました:', error)
+        print("Instagram APIのリクエスト中にエラーが発生しました:", error)
     return None
+
 
 def search_top_hash_tag_posts(business_id, hash_tag_id, access_token):
     """指定したハッシュタグIDのトップ投稿を検索する。"""
-    url = f'https://graph.facebook.com/{hash_tag_id}/recent_media?user_id={business_id}&fields=caption,comments_count,id,like_count,media_type,media_url,permalink,timestamp,children%7Bmedia_url%7D&access_token={access_token}&limit=35'
+    url = f"https://graph.facebook.com/{hash_tag_id}/recent_media?user_id={business_id}&fields=caption,comments_count,id,like_count,media_type,media_url,permalink,timestamp,children%7Bmedia_url%7D&access_token={access_token}&limit=35"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        instagram_media = data.get('data')
+        instagram_media = data.get("data")
         print(instagram_media)  # データ出力
         return instagram_media
     except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
+        print(f"HTTP error occurred: {http_err}")
     except Exception as error:
-        print('Instagram APIのリクエスト中にエラーが発生しました:', error)
+        print("Instagram APIのリクエスト中にエラーが発生しました:", error)
     return None
+
 
 def main():
     settings = load_settings("auth.json")
     hash_tag_name = "ラーメン"
     insta_business_id = settings["insta_business_id"]
     access_token = settings["access_token"]
-    
+
     hash_tag_id = get_ig_hash_id(insta_business_id, hash_tag_name, access_token)
     if hash_tag_id:
         search_top_hash_tag_posts(insta_business_id, hash_tag_id, access_token)
+
 
 if __name__ == "__main__":
     main()
--- ranking/rankingapp/models.py
+++ ranking/rankingapp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class RankingModel(models.Model):
     shop_name = models.CharField(max_length=100)
-    # shop_location = models.CharField(max_length=100)
+    shop_location = models.CharField(max_length=100)
     # shop_image = models.ImageField(upload_to='images/')
-    # buisiness_hours = models.CharField(max_length=100)
+    buisiness_hours = models.CharField(max_length=100)
     # created_data = models.DateTimeField(auto_now_add=True)
     # modified_date = models.DateTimeField(auto_now=True, blank=True, null=True)
     # shop_type_id = models.CharField(max_length=10, blank=True, null=True) #後で外部キーに変更

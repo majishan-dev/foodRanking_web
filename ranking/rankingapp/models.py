f292720b9642f9933d498afa00d8e2c3397706b3
hiropau <hiropay28@gmail.com>
2024-01-27 12:27:13 +0900

    モデル要素追加

--- ranking/rankingapp/models.py
+++ ranking/rankingapp/models.py
@@ -7,8 +7,8 @@ class RankingModel(models.Model):
     shop_location = models.CharField(max_length=100)
     # shop_image = models.ImageField(upload_to='images/')
     buisiness_hours = models.CharField(max_length=100)
-    # created_data = models.DateTimeField(auto_now_add=True)
-    # modified_date = models.DateTimeField(auto_now=True, blank=True, null=True)
+    created_data = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True, blank=True, null=True)
     # shop_type_id = models.CharField(max_length=10, blank=True, null=True) #後で外部キーに変更
     # 例models.ForeignKey(Category, on_delete=models.CASCADE)
 

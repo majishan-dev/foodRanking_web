341696d56e6efcd444e3054a08b4a89a4044d7a9
hiropau <hiropay28@gmail.com>
2024-01-27 16:01:49 +0900

    関数追加

--- ranking/rankingapp/urls.py
+++ ranking/rankingapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoList, RankingHome, RankingList
+from .views import TodoList, RankingHome, RankingList, RankList
 
 urlpatterns = [
-    path('list/', TodoList, name='list'),
-    path('list_test/', RankingList.as_view(), name='list_test'),
-    path('home/', RankingHome.as_view(), name='home'),
+    path("list/", TodoList, name="list"),
+    path("list_test_func/", RankList, name="list_test"),
+    path("list_test/", RankingList.as_view(), name="home"),
+    path("home/", RankingHome.as_view(), name="home"),
 ]
--- ranking/rankingapp/views.py
+++ ranking/rankingapp/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import ListView,TemplateView
+from django.views.generic import ListView, TemplateView
 from .models import RankingModel
 
 # Create your views here.
 
-#classbasedよりはfunctionbasedの方がよいとおもっているが認識合う？
+# classbasedよりはfunctionbasedの方がよいとおもっているが認識合う？
+
 
 def TodoList(request):
     range_list = range(2)
-    return render(request, 'list.html', {'range_list': range_list})
+    return render(request, "list.html", {"range_list": range_list})
+
 
 class RankingList(ListView):
-    template_name = 'rankinglist.html'
+    template_name = "rankinglist.html"
     model = RankingModel
 
+
+def RankList(request):
+    object_list = RankingModel.objects.all()
+    print(object_list)
+    return render(request, "list.html", {"object_list": object_list})
+
+
 class RankingHome(TemplateView):
-    template_name = 'home.html'
-    
+    template_name = "home.html"

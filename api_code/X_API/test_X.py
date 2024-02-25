82150c060dbf614db5c980654904312f294f2b87
hiropau <hiropay28@gmail.com>
2024-02-25 19:33:18 +0900

    tweepy修正

--- api_code/X_API/test_X.py
+++ api_code/X_API/test_X.py
@@ -1,18 +1,17 @@
 import tweepy
 
 # APIキーとアクセストークンの設定
-api_key='XXX'
-api_key_secret='XXX'
-access_token='XXX'
-access_token_secret='XXX'
+api_key = "dacvLO8mPZm5GkKAhwmg4fk2M"
+api_key_secret = "rmkOggB1CNFeJOllkweVQx11mgPgneMuiQh8uSEhLEdUIf4xpM"
+access_token = "849501462011023360-2sVFHcjhSPQdICYOqJtrMhrm5k2YnW9"
+access_token_secret = "jMA1Lp9ZOws4bbbGXzqkEdQnBxAOkBPaI72hwK5SPrHa0"
 
 client = tweepy.Client(
     consumer_key=api_key,
     consumer_secret=api_key_secret,
     access_token=access_token,
-    access_token_secret=access_token_secret
+    access_token_secret=access_token_secret,
 )
 
 # 投稿
 client.create_tweet(text="テスト")
-
